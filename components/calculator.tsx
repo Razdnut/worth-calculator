591a06db751da875dfe13eaabb77cfa155d2a05f
Razdnut <andor@venclous.org>
2025-10-19 12:24:37 +0200

    Remove forced redirect to worthjob

--- components/calculator.tsx
+++ components/calculator.tsx
@@ -396,14 +396,6 @@ const SalaryCalculator = () => {
   // 在组件挂载时标记为浏览器环境
   useEffect(() => {
     setIsBrowser(true);
-    
-    // 在客户端环境中执行重定向
-    if (typeof window !== 'undefined') {
-      const hostname = window.location.hostname;
-      if (hostname !== 'worthjob.zippland.com' && hostname !== 'localhost' && !hostname.includes('127.0.0.1')) {
-        window.location.href = 'https://worthjob.zippland.com' + window.location.pathname;
-      }
-    }
   }, []);
   
   // 添加用于创建分享图片的引用
